7e355370b86f6af72ce25b15ebf478ee32204c16
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-14 16:45:38 -0300

    Processo de arrastar 100% concluido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,40 @@
-document.querySelectorAll('.item').forEach(item =>{
-   item.addEventListener('dragstart',dragStart);
-   item.addEventListener('dragend',dragEnd);
-})
+document.querySelectorAll(".item").forEach((item) => {
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
+});
 
-document.querySelectorAll('.area').forEach(area =>{
-   area.addEventListener('dragover', dragOver);
-   area.addEventListener('dragleave', dragLeaver);
-   area.addEventListener('drop', drop);
-
-
-})
+document.querySelectorAll(".area").forEach((area) => {
+  area.addEventListener("dragover", dragOver);
+  area.addEventListener("dragleave", dragLeaver);
+  area.addEventListener("drop", drop);
+});
 
 // ------------FUNÇÕES--------
 
 // --Item arrastado
-function dragStart(e){
-   e.currentTarget.classList.add('dragging');
+function dragStart(e) {
+  e.currentTarget.classList.add("dragging");
 }
 
-function dragEnd(e){
-  e.currentTarget.classList.remove('dragging');
+function dragEnd(e) {
+  e.currentTarget.classList.remove("dragging");
 }
 
 // -- Area p/ soltar
-function dragOver(e){
-   e.preventDefault();
-   e.currentTarget.classList.add('hover');
+function dragOver(e) {
+  if (e.currentTarget.querySelector(".item") === null) {
+    e.preventDefault();
+    e.currentTarget.classList.add("hover");
+  }
 }
-function dragLeaver(e){
-   e.currentTarget.classList.remove('hover');
+function dragLeaver(e) {
+  e.currentTarget.classList.remove("hover");
 }
-function drop(e){
-   e.currentTarget.classList.remove('hover');
-   
-   let dragItem = document.querySelector('.item.dragging')
-   if(e.currentTarget.querySelector('.item') === null){
-      e.currentTarget.appendChild(dragItem);
-   }
-   
+function drop(e) {
+  e.currentTarget.classList.remove("hover");
+
+  if (e.currentTarget.querySelector(".item") === null) {
+    let dragItem = document.querySelector(".item.dragging");
+    e.currentTarget.appendChild(dragItem);
+  }
 }
